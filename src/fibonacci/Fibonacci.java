8a9c4c7189cef308f53a96e2b4b7080fa8ff4e54
Mussolenguini <43378571+Mussolenguini@users.noreply.github.com>
2020-03-23 14:37:20 -0400

    Update Fibonacci.java

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -18,19 +18,26 @@ public class Fibonacci {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //get input
         int input = Integer.parseInt(JOptionPane.showInputDialog("Enter an integer to find its fibonacci number"));
+        //find output
         int output = fibonacci(input);
+        //display output
         JOptionPane.showMessageDialog(null, "The fibonacci of " + input + " is: " + output);
         
     }
+    //Method for finding the fibonacci
     public static int fibonacci(int num) {
+        //Return 0 if input results in 0
         if (num == 0) {
             return(0);
         }
+        //Use recursion to return the added sum on all number prior to input
         else if (num > 2) {
             return(fibonacci(num-1) + fibonacci(num-2));
         }
         else {
+        //return 1 if its the 1st and 2nd digit of the fibonacci sequence
             return(1);
         }
     }
